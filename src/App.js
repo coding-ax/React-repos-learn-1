a25086d98c8660ce39b339f3def891b9353fc6ad
whutfaker <1312976124@qq.com>
2020-06-08 18:02:12 +0800

    NONE

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import { Input, Button } from 'antd'
+import 'antd/dist/antd.css'
+class App extends React.Component {
+  state = {
+    val: "",
+    list: [],
+  };
+  handleChange = (event) => {
+    let val = event.target.value;
+    this.setState({
+      val,
+    });
+    // console.log(val)
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleAdd = () => {
+    let list = this.state.list;
+    list.push(this.state.val);
+    this.setState({
+      val: "",
+      list,
+    });
+  };
+  render() {
+    let listItem = this.state.list.map((item, index) => {
+      let li = (
+        <li key={index}>
+          {item}
+        </li>
+      );
+      return li;
+    });
+    const width = {
+      width: 300
+    }
+    return (
+      <div className="App">
+        <h1>hello world</h1>
+        <p>welcome to React's World！</p>
+        <Input
+          style={width}
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.val}
+        />
+        <Button onClick={this.handleAdd}>添加</Button>
+        <ul>{listItem}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
